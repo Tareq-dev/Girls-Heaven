c51cdc00a70166f372ac893c30c75aa08aeabcb3
Tarequl Islam <tareque.dev@gmail.com>
2022-03-13 16:58:26 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,158 @@
+const data =[
+     {
+         id: 1,
+         image: "./images/bg-b1.jpg",
+          title: "Lorem, ipsum dolor sit amet",
+          price: 780,
+         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+     },
+     {
+          id: 2,
+          image: "./images/bg-b2.jpg",
+           title: "Lorem, ipsum dolor sit amet",
+          price: 780,
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },
+      {
+          id: 3,
+          image: "./images/bg-b3.jpg",
+           title: "Lorem, ipsum dolor sit amet",
+          price: 780,
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },
+      {
+          id: 4,
+          image: "./images/bg-b4.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },
+      {
+          id: 5,
+          image: "./images/bg-b5.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 6,
+          image: "./images/bg-b6.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 7,
+          image: "./images/bg-b7.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 8,
+          image: "./images/bg-b8.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 9,
+          image: "./images/bg-b9.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 10,
+          image: "./images/bg-b10.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 11,
+          image: "./images/bg-b11.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 12,
+          image: "./images/bg-b12.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 13,
+          image: "./images/bg-b13.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 14,
+          image: "./images/bg-b14.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 15,
+          image: "./images/bg-b15.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 16,
+          image: "./images/bg-b16.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 17,
+          image: "./images/bg-b17.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 18,
+          image: "./images/bg-b18.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 19,
+          image: "./images/bg-b19.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      },{
+          id: 20,
+          image: "./images/bg-b20.jpg",
+          price: 780,
+          title: "Lorem, ipsum dolor sit amet",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi reiciendis vitae non, eos illum fugit tempore dolor deleniti architecto porro accusantium. Vel aliquid, minus obcaecati voluptatum vero suscipit consectetur!"
+      }
+ ]
+
+data.forEach(product => {
+     const myJwelary = document.getElementById('myJwelary');
+     const div = document.createElement('div');
+     div.innerHTML=`
+     <div class="col">
+     <div class="card h-100">
+     <img style="height:250px;" src="${product.image}" class="card-img-top" alt="...">
+     <div class="card-body">
+          <h3 class="text-info">$${product.price}</h3>
+          <h5 class="card-title">${product.title}</h5>
+          <p class="card-text content">${product.description}</p>
+     </div>
+     <div class="mx-2 d-flex justify-content-between">
+          <div class="d-flex flex-row justify-content-start mt-1 mb-2 text-warning fs-4">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+          </div>
+          <div class="justify-content-end mb-3">
+               <button class="border-0 bg-success text-light fw-bold rounded-3 py-1">Buy Now</button>
+          </div>
+        </div>
+     </div>
+     </div>
+     `
+     myJwelary.appendChild(div)
+})
+
